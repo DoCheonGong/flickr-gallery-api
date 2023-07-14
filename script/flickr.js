50821b58851ab0a8ea84e73c3a7c1676697da777
DoCheonGong <126742693+DoCheonGong@users.noreply.github.com>
2023-07-14 10:38:02 +0900

    2023.07.12 - Exception

--- script/flickr.js
+++ script/flickr.js
@@ -14,16 +14,16 @@ const searchBtn = document.querySelector(".btnSearch");
 const url = `${base}method=${method}&api_key=${key}&per_page=${per_page}&format=${format}&nojsoncallback=1`;
 const url2 = `${base}method=${method2}&api_key=${key}&per_page=${per_page}&format=${format}&nojsoncallback=1&tags=Apple&privacy_filter=1`;
 
-callData(url2);
-searchBtn.addEventListener("click", () => {
-    // 버튼을 클릭하면, input 태그에 사용자가 넣은 값을 가져와서
-    // URL 안의 tags에 넣고 해당 URL을 callData()의 인수자리에 넣어서
-    // 해당 값을 검색하도록 하는 코딩을 적는다
-    let tag = input.value;
-    const url = `${base}method=${method2}&api_key=${key}&per_page=${per_page}&format=${format}&nojsoncallback=1&tags=${tag}&privacy_filter=1`;
+/*
+let a = document.querySelector(".item a");
+console.log(a); // null
 
-    callData(url);
-})
+item class는 동적으로 생성된 요소이므로 querySelector로 직접 참조를 할 수 없다
+해결책: 이벤트 위임
+실제로 존재하는 부모 태그인 #list에 이벤트를 위임해서 클릭 이벤트 전달 
+*/
+
+callData(url2);
 
 function callData(url) {
     frame.innerHTML = "";
@@ -37,6 +37,76 @@ function callData(url) {
         let items = json.photos.photo;
         createList(items);
         delayLoading();
+        searchBtn.addEventListener("click", () => {
+            // 버튼을 클릭하면, input 태그에 사용자가 넣은 값을 가져와서
+            // URL 안의 tags에 넣고 해당 URL을 callData()의 인수자리에 넣어서
+            // 해당 값을 검색하도록 하는 코딩을 적는다
+            let tag = input.value;
+            tag = tag.trim();
+            const url = `${base}method=${method2}&api_key=${key}&per_page=${per_page}&format=${format}&nojsoncallback=1&tags=${tag}&privacy_filter=1`;
+
+            if (tag != "") {
+                if (!items.length == 0) {
+                    callData(url);
+                } else {
+                    frame.innerHTML = ""; // 높이값을 초기화하기 위한 빈 HTML
+                    frame.style.height = "auto";
+                    frame.classList.remove("on");
+
+                    let errMsgs = document.createElement("p");
+                    errMsgs.append("검색 결과가 없습니다. 검색어를 확인해주세요.");
+                    frame.parentElement.append(errMsgs);
+                }
+            } else {
+                // 로딩바를 없애고 높이값을 초기화하고 p태그를 만들어서
+                // 경고문구를 출력한다
+                frame.innerHTML = ""; // 높이값을 초기화하기 위한 빈 HTML
+                frame.style.height = "auto";
+                frame.classList.remove("on");
+
+                let isErrMsgs = frame.parentElement.querySelectorAll("p");
+                if (isErrMsgs);
+                let errMsgs = document.createElement("p");
+                errMsgs.append("검색어가 없습니다. 검색어를 입력해주세요.");
+                frame.parentElement.append(errMsgs); // 부모 요소를 모를 때
+                // frame.closest("#wrap").append(errMsgs); // 부모 요소를 알고 있을 때
+            }
+            // tag의 값이 없을 경우 "검색어가 없습니다. 검색어를 입력해주세요." 라는
+            // 경고문구를 출력한다
+        })
+
+        input.addEventListener("keyup", (e) => {
+            if (e.key === "Enter") {
+                let tag = input.value;
+                const url = `${base}method=${method2}&api_key=${key}&per_page=${per_page}&format=${format}&nojsoncallback=1&tags=${tag}&privacy_filter=1`;
+
+                if (tag != "") {
+                    if (!items.length == 0) {
+                        callData(url);
+                    } else {
+                        frame.innerHTML = ""; // 높이값을 초기화하기 위한 빈 HTML
+                        frame.style.height = "auto";
+                        frame.classList.remove("on");
+
+                        let errMsgs = document.createElement("p");
+                        errMsgs.append("검색 결과가 없습니다. 검색어를 확인해주세요.");
+                        frame.parentElement.append(errMsgs);
+                    }
+                } else {
+                    frame.innerHTML = "";
+                    frame.style.height = "auto";
+                    frame.classList.remove("on");
+
+                    let errMsgs = document.createElement("p");
+                    errMsgs.remove();
+                    errMsgs.append("검색어가 없습니다. 검색어를 입력해주세요.");
+                    frame.parentElement.append(errMsgs);
+                }
+            } // Enter 키를 누르면 검색
+            // if(e.keyCode == 13)
+        })
+        // keyup: 키를 누르자 마자
+        // keypress: 키를 누르고 뗀 뒤
     })
 }
 
@@ -77,7 +147,7 @@ function delayLoading() {
             if (count == len) isoLayout();
         })
 
-        el.addEventListener("error", () => {
+        el.addEventListener("error", (e) => {
             // 1. 오류가 발생하는 대상 지우기 (권장하지 않음)
             /* 
                 e.currentTarget.closet(".item").style.display = "none";
